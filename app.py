866576933f5abaa42effcdcfb75c3d3f98ef354d
Vishwamohan Singh Negi <vishwamohansinghnegi@gmail.com>
2024-04-22 18:00:00 +0530

    Commited on fucntion branch

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from calc_func import do_addition  , do_subtraction
+from multiply import do_multiply
 def main():
     print("Welcome to calculator")
     a=int(input('Enter value of a : '))
     b=int(input('Enter value of b : '))
     print('''Select the function from the given options
         1) Addition 
-        2)Subtraction''')
+        2)Subtraction
+        3)Multilpy''')
     inp = input("Enter input : ")
     if inp=='1':
         result = do_addition(a,b)
     elif inp=='2':
         result = do_subtraction(a,b)
+    elif inp=='3':
+        result = do_multiply(a,b)
     print('Result : ', result)
 if __name__=='__main__':
     main()
---
+++ multiply.py
@@ -0,0 +1,2 @@
+def do_multiply(a:int ,b:int):
+    return a*b
